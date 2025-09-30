8efc221db75b3ac2f0d8f8bdf9ea7781898f6980
Adefolarin.. <suberufolarin@gmail.com>
2025-09-30 20:56:19 +0100

    Completed the tab icon

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,37 +1,70 @@
 import { Tabs } from "expo-router"
-import { Image, View,Text, Dimensions } from "react-native"
+import { Image, View,Text, Dimensions, StyleSheet } from "react-native"
 import { COLORS, SIZES } from "../../theme/theme"
+import AntDesign from '@expo/vector-icons/AntDesign';
 
+
+
+const TabIcon = ({focused , name , image})=>{
+    return(
+        <View style={styles.tabView}>
+
+            <Image tintColor={focused ? COLORS.red: COLORS.grey} style={styles.tabIcon} source={image}/>
+            {
+                focused ? <Image tintColor={COLORS.red} style={{height:8, width:8}} source={require('../../assets/images/dot.png')}/>
+                : 
+                <Text style={styles.tabText}>{name}</Text>
+            }
+        </View>
+    )
+}
 
 const TabLayout = ()=>{
     return(
-        <Tabs screenOptions={{headerShown:false, tabBarStyle:{height:88, }}}>
-            <Tabs.Screen name="home" 
-            options={{
-                tabBarIcon : ({focused})=> {
-                    return(
-                        <View style={{alignItems:'center', gap:10, marginTop:20, width:Dimensions.get('screen').width*.25, alignSelf:'center'}}>
-                            <Image source={require('../../assets/images/home.png')} tintColor={focused ? COLORS.red : COLORS.grey} style={{height:24, width:24}} />
+        <Tabs screenOptions={{headerShown:false, tabBarStyle:{height:88,}}}>
 
-                            {focused ? <Image source={require('../../assets/images/dot.png')} tintColor={COLORS.red} style={{height:8, width:8}}/> : <Text style={{fontFamily:'medium', color:COLORS.black, fontSize:SIZES.small}}>Home</Text>}
+            <Tabs.Screen options={{
+                title:'',
+                tabBarIcon: ({focused}) => <TabIcon focused={focused} name={'Home'} image={require('../../assets/images/home.png')}/>
+            }} name="home"/>
 
-                        </View>
-                    )
+            <Tabs.Screen  options={{
+                title:'',
+                tabBarIcon: ({focused}) => <TabIcon focused={focused} name={'Transactions'} image={require('../../assets/images/transaction.png')}/>
+            }} name="transactions"/>
 
-                },
+            <Tabs.Screen  options={{
+                title:'',
+                tabBarIcon: ({focused}) => <TabIcon focused={focused} name={'Referrals'} image={require('../../assets/images/gift.png')}/>
+            }} name="referrals"/>
 
-                title:''
+            <Tabs.Screen 
+        
+             options={{
+                title:'',
+                tabBarIcon: ({focused}) => <TabIcon focused={focused} name={'Account'} image={require('../../assets/images/account.png')}/>
             }}
+            
+            name="account"/>
 
-            
-            
-            />
-            <Tabs.Screen name="transactions"/>
-            <Tabs.Screen name="referrals"/>
-            <Tabs.Screen name="account"/>
         </Tabs>
     )
 }
 
 
 export default TabLayout;
+
+
+const styles = StyleSheet.create({
+    tabView:{
+        alignItems:'center',  width:Dimensions.get('screen').width * 0.25, marginTop:20, gap:8
+
+    },
+    tabIcon :{width:24, height:24},
+    tabText:{
+        fontFamily:'medium',
+        color:COLORS.grey,
+        fontSize:SIZES.small
+
+    }
+})
